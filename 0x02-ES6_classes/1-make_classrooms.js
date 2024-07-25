9d8ccbb86f7e87e54574b23d68486d78ba40bbf9
DentistOmarAshraf <omar_ashraf@live.com>
2024-07-26 01:46:05 +0300

    1-make_classrooms.js

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,9 @@
+import ClassRoom from './0-classroom';
+
+export default function initializRooms() {
+  const arrayOfClassroom = [];
+  arrayOfClassroom.push(new ClassRoom(19));
+  arrayOfClassroom.push(new ClassRoom(20));
+  arrayOfClassroom.push(new ClassRoom(34));
+  return (arrayOfClassroom);
+}
