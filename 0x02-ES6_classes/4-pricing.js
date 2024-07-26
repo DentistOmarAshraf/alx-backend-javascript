e1d98681a5ef42fcbc0fc7882f1a820abb2a765a
DentistOmarAshraf <omar_ashraf@live.com>
2024-07-26 04:03:06 +0300

    Static method added !

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -31,4 +31,14 @@ export default class Pricing {
   displayFullPrice() {
     return `${this.amount} ${this.currency.displayFullCurrency()}`;
   }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof (amount) !== 'number') {
+      throw TypeError('Amount must be number');
+    }
+    if (typeof (conversionRate) !== 'number') {
+      throw TypeError('ConversionRate must be a number');
+    }
+    return (amount * conversionRate);
+  }
 }
