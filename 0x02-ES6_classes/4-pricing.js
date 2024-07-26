a5246633113af296cc883d02ea09ab35262bffaa
DentistOmarAshraf <omar_ashraf@live.com>
2024-07-26 03:41:22 +0300

    4-pricing.js

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,34 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof (value) !== 'number') {
+      throw TypeError('Amount must be a Number');
+    }
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw TypeError('Currency must be instance of Currency');
+    }
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
+  }
+}
