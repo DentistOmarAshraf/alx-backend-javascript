bb3d60eefc5946f17c20545b28e583cef3c82172
DentistOmarAshraf <omar_ashraf@live.com>
2024-07-27 15:55:30 +0300

    Try Promise.all()

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,10 @@
 import { createUser, uploadPhoto } from './utils';
 
 export default function handleProfileSignup() {
-  const photo = uploadPhoto();
-  const user = createUser();
-  photo
-    .then((data) => data)
-    .then((resp) => {
-      user
-        .then((data) => console.log(`${resp.body} ${data.firstName} ${data.lastName}`));
+  Promise.all([uploadPhoto(), createUser()])
+    .then((data) => {
+      const allData = `${data[0].body} ${data[1].firstName} ${data[1].lastName}`;
+      console.log(allData);
     })
-    .catch(() => console.log('Signup system offline'));
+    .catch(() => { console.log('Signup system offline'); });
 }
