344f16166768d9c72f7fa2264f383b41dd6c4e4f
DentistOmarAshraf <omar_ashraf@live.com>
2024-07-27 05:12:49 +0300

    try

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,14 +2,15 @@
 export default function handleResponseFromAPI(promise) {
   promise
     .then((response) => {
+      console.log('Got a response from the API');
       const obj = {
         status: 200,
         body: 'success',
       };
       return obj;
     })
-    .catch((err) => new Error(''))
-    .finally(() => {
+    .catch((err) => {
       console.log('Got a response from the API');
+      return new Error('');
     });
 }
