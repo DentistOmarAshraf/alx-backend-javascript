bf7a998d0c7ac54aa09087ae9f40ab11fb94ed0f
DentistOmarAshraf <omar_ashraf@live.com>
2024-07-29 23:01:29 +0300

    ---

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,7 @@
 export default function getListStudents() {
-  const arrayOfStudent = [];
-  arrayOfStudent.push({ id: 1, firstName: 'Guillamume', lastName: 'San Francisco' });
-  arrayOfStudent.push({ id: 2, firstName: 'James', location: 'Columbia' });
-  arrayOfStudent.push({ id: 5, firstName: 'Serena', lastName: 'San Francisco' });
-  return arrayOfStudent;
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
 }
