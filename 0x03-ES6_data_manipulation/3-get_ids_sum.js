1b08e5d1a5291221dff66cc321c52f5014ae2660
DentistOmarAshraf <omar_ashraf@live.com>
2024-07-29 16:27:26 +0300

    3-get_ids_sum.js

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,6 @@
 export default function getStudentIdsSum(obj) {
   if (!(Array.isArray(obj))) return [];
   const arrayOfStudentIds = obj.map((elem) => elem.id);
-  const sumsOfIds = arrayOfStudentIds.reduce((x, y) => x + y);
+  const sumsOfIds = arrayOfStudentIds.reduce((x, y) => x + y, 0);
   return sumsOfIds;
 }
