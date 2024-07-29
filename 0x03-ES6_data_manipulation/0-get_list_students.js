6af96ce3cb28b03b46cf0b4478e8a8219f6fcb68
DentistOmarAshraf <omar_ashraf@live.com>
2024-07-29 22:58:57 +0300

    [Edit] 1-get_list_students

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,15 +1,7 @@
-class Student {
-  constructor(id, firstName, location) {
-    this.id = id;
-    this.firstName = firstName;
-    this.location = location;
-  }
-}
-
 export default function getListStudents() {
   const arrayOfStudent = [];
-  arrayOfStudent.push(new Student(1, 'Guillamume', 'San Francisco'));
-  arrayOfStudent.push(new Student(2, 'James', 'Columbia'));
-  arrayOfStudent.push(new Student(5, 'Serena', 'San Francisco'));
+  arrayOfStudent.push({ id: 1, firstName: 'Guillamume', lastName: 'San Francisco' });
+  arrayOfStudent.push({ id: 2, firstName: 'James', location: 'Columbia' });
+  arrayOfStudent.push({ id: 5, firstName: 'Serena', lastName: 'San Francisco' });
   return arrayOfStudent;
 }
