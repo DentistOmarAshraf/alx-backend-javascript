bd39c70be4245fa368c35be734c1c83273977daf
DentistOmarAshraf <omar_ashraf@live.com>
2024-07-31 19:39:34 +0300

    ----

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (startString === '' || startString.length === 0) return ('');
+  if (startString === '' || startString.length === 0) return (' ');
   const filterdStrings = [];
   set.forEach((elem) => {
     if (elem.startsWith(startString)) {
