fa56a6612996317f5fe0bcfc1d81ac0f2b815033
DentistOmarAshraf <omar_ashraf@live.com>
2024-07-31 14:45:10 +0300

    7-has_array_values.js

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,4 +1,5 @@
 export default function hasValuesFromArray(set, array) {
+  if (array.length === 0) return false;
   let bool = true;
   array.forEach((elem) => {
     if (!(elem in Array.from(set))) bool = false;
