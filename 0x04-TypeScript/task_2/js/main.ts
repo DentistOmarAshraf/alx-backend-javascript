a747e84769050e90698a63cb9143d4d515097cfd
DentistOmarAshraf <omar_ashraf@live.com>
2024-08-03 04:52:29 +0300

    [Add] isDierctor()=>{} executeWork()=>{}

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -43,13 +43,34 @@ interface createEmployeeFunction {
 
 const createEmployee: createEmployeeFunction = (salary) => {
     let theSalary = 0;
-    if (typeof (salary) == 'string'){
+    if (typeof (salary) == 'string') {
         theSalary += parseInt(salary.slice(1));
-    }else {
-        theSalary +=  salary
+    } else {
+        theSalary += salary
     }
 
     if (theSalary < 500)
         return new Teacher()
     return new Director()
+}
+
+function isDirector(employee: Teacher | Director): employee is Director {
+    return (employee as Director).workDirectorTasks !== undefined;
+}
+
+function isTeacher(employee: Teacher | Director): employee is Teacher {
+    return (employee as Teacher).workTeacherTasks !== undefined;
+}
+
+interface executeWorkFunction {
+    (employee: Teacher | Director): void;
+}
+
+const executeWork: executeWorkFunction = (employee) => {
+    if (isDirector(employee)) {
+        console.log(employee.workDirectorTasks())
+    }
+    if (isTeacher(employee)) {
+        console.log(employee.workTeacherTasks())
+    }
 }
