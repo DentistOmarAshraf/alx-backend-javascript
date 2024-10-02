9823f1434a242e0ba9356279b5ebe41e7913f5fd
DentistOmarAshraf <omar_ashraf@live.com>
2024-10-02 05:07:21 +0300

    1-stdin.js

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.log('Welcome to Holberton School, what is your name?');
+
+rl.on('line', (input) => {
+  console.log(`Your name is: ${input}`);
+  console.log('This important software is now closing');
+  rl.close(); // Close the readline interface
+});
