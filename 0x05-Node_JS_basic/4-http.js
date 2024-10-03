40f93f07b8b17b22b59c38f650416fe72d5eaee3
DentistOmarAshraf <omar_ashraf@live.com>
2024-10-03 19:59:33 +0300

    4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,13 +2,15 @@
 
 const { createServer } = require('node:http');
 
-const hostname = '127.0.0.1';
+const hostname = 'localhost';
 const port = 1245;
 
 const app = createServer((req, res) => {
+  const responseText = 'Hello Holberton School!';
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!');
+  res.setHeader('Content-length', responseText.length);
+  res.write(Buffer.from(responseText));
 });
 
 app.listen(port, hostname);
