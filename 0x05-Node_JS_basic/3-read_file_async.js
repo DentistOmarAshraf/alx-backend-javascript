7f788717ed07dce43ddb1d5ed82335d177bc0737
DentistOmarAshraf <omar_ashraf@live.com>
2024-10-03 13:06:03 +0300

    3-read_file_async.js

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+
+function countStudents(path) {
+  const myPromise = new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      }
+      const csvArray = data.split('\n');
+      const columns = csvArray.splice(0, 1)[0].split(',');
+      const arrOfStudent = [];
+      const uniqueFields = new Set();
+      for (const st of csvArray) {
+        if (st === '') break;
+        const stData = st.split(',');
+        const newStudent = {};
+        for (let i = 0; i < columns.length; i += 1) {
+          newStudent[columns[i]] = stData[i];
+        }
+        arrOfStudent.push(newStudent);
+        uniqueFields.add(newStudent.field);
+      }
+      console.log(`Number of students: ${arrOfStudent.length}`);
+      for (const field of uniqueFields) {
+        const tempArr = [];
+        for (const st of arrOfStudent) {
+          if (st.field === field) tempArr.push(st.firstname);
+        }
+        console.log(`Number of students in ${field}: ${tempArr.length}. List: ${tempArr.join(', ')}`);
+        resolve('Done');
+      }
+    });
+  });
+
+  return myPromise;
+}
+
+module.exports = countStudents;
