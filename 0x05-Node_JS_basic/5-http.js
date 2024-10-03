b7b0e4edec3d58b9cb0ca02b3d69402d92b8bc3f
DentistOmarAshraf <omar_ashraf@live.com>
2024-10-03 20:41:59 +0300

    5-http.js

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+const { createServer } = require('http');
+const countStudents = require('./3-read_file_async');
+
+const host = 'localhost';
+const port = 1245;
+
+const app = createServer((req, res) => {
+  if (req.url === '/' && req.method === 'GET') {
+    const text = 'Hello Holberton School!';
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Length', text.length);
+    res.end(text);
+  }
+  if (req.url === '/students' && req.method === 'GET') {
+    const path = process.argv[2];
+    const originalLog = console.log;
+    let output = '';
+    console.log = (data) => {
+      output += `${data}\n`;
+    };
+    console.log('This is the list of our students');
+    countStudents(path)
+      .then(() => res.end(output))
+      .catch((err) => {
+        const text = err.message;
+        res.statusCode = 500;
+        res.end(`${text}\n`);
+      });
+
+    console.log = originalLog;
+  }
+});
+
+app.listen(port, host, () => {
+  console.log(`Server Run on ${host}:${port}`);
+});
