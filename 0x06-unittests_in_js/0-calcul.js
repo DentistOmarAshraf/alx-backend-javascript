6f249b5a31fd270ab77f0a304684d7cbf47fc1dd
DentistOmarAshraf <omar_ashraf@live.com>
2024-10-04 03:52:51 +0300

    0-calcul.js

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,11 @@
+/**
+ * calculateNumber - sum of round of a and b
+ * @param {number} a 
+ * @param {number} b 
+ * @returns number
+ */
+const calculateNumber = (a, b) => {
+  return Math.round(a) + Math.round(b)
+};
+
+module.exports = calculateNumber;
---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,18 @@
+
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber', function () {
+  it('assert Equal', function () {
+    assert.equal(calculateNumber(1, 3), 4);
+  })
+  it('assert Equal', function () {
+    assert.equal(calculateNumber(1, 3.7), 5);
+  })
+  it('assert Equal', function () {
+    assert.equal(calculateNumber(1.2, 3.7), 5);
+  })
+  it('assert Equal', function () {
+    assert.equal(calculateNumber(1.5, 3.7), 6);
+  })
+})
