cb48227298fe68f976da21ca41a7ca32ee9f7c4c
DentistOmarAshraf <omar_ashraf@live.com>
2024-10-04 03:59:47 +0300

    0-calcul.test.js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -15,4 +15,19 @@ describe('calculateNumber', function () {
   it('assert Equal', function () {
     assert.equal(calculateNumber(1.5, 3.7), 6);
   })
+  it('assert Equal', function () {
+    assert.equal(calculateNumber(1.4999, 3.499), 4);
+  })
+  it('assert Equal', function () {
+    assert.equal(calculateNumber(-1.5, -2.5), -3);
+  })
+  it('assert Equal', function () {
+    assert.equal(calculateNumber(Infinity, 2), Infinity);
+  })
+  it('assert Equal', function () {
+    assert.equal(calculateNumber(NaN, 2), NaN);
+  })
+  it('assert Equal', function () {
+    assert.equal(calculateNumber(123456789.5, 0.499999), 123456790);
+  })
 })
