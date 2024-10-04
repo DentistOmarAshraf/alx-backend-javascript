f884f32fc2f3f68240e9bfde5de8bad30f112f14
DentistOmarAshraf <omar_ashraf@live.com>
2024-10-04 04:06:48 +0300

    0-calcul.test.js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,27 +4,21 @@ const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', function () {
   it('assert Equal', function () {
-    assert.equal(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1, 3), 4);
   })
   it('assert Equal', function () {
-    assert.equal(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
   })
   it('assert Equal', function () {
-    assert.equal(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
   })
   it('assert Equal', function () {
-    assert.equal(calculateNumber(1.5, 3.7), 6);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   })
   it('assert Equal', function () {
-    assert.equal(calculateNumber(1.4999, 3.499), 4);
+    assert.strictEqual(calculateNumber(2.4999, 3.4999), 5);
   })
   it('assert Equal', function () {
-    assert.equal(calculateNumber(-1.5, -2.5), -3);
-  })
-  it('assert Equal', function () {
-    assert.equal(calculateNumber(Infinity, 2), Infinity);
-  })
-  it('assert Equal', function () {
-    assert.equal(calculateNumber(NaN, 2), NaN);
+    assert.strictEqual(calculateNumber(-1.5, -2.5), -3);
   })
 })
