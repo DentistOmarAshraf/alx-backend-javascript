ef8cddbf9a7f39cb61c578bfb18082ee51e1e6ad
DentistOmarAshraf <omar_ashraf@live.com>
2024-10-05 08:30:20 +0300

    4-payment.js

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,26 @@
+
+const sinon = require('sinon');
+const { expect } = require('chai');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  it('Checking usage of Utils.calculateNumber', () => {
+    const spy = sinon.spy(Utils, 'calculateNumber');
+    sendPaymentRequestToApi(10, 20);
+    expect(spy.calledOnce).to.be.true;
+    expect(spy.returnValues).to.deep.equal([30]);
+    expect(spy.args[0]).to.deep.equal(['SUM', 10, 20])
+  });
+  it('Stub Utils.calculateNumber', () => {
+    const stub = sinon.stub(Utils, 'calculateNumber').returns(10)
+    const spyLog = sinon.spy(console, "log")
+    sendPaymentRequestToApi(100, 20);
+    expect(stub.returnValues).to.deep.equal([10]);
+    expect(stub.calledOnce).to.be.true;
+    expect(spyLog.calledWith('The total is: 10')).to.be.true;
+  });
+  afterEach(() => {
+    sinon.restore();
+  })
+})
