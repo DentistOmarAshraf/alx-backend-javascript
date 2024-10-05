6f8981c7e4f7d0dbfa12fb4ff484172ac729b61b
DentistOmarAshraf <omar_ashraf@live.com>
2024-10-05 17:42:41 +0300

    ---

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -11,17 +11,19 @@ describe('sendPaymentRequestToApi', () => {
     expect(spy.calledOnce).to.be.true;
     expect(spy.returnValues).to.deep.equal([30]);
     expect(spy.args[0]).to.deep.equal(['SUM', 10, 20])
+    spy.restore()
   });
   it('Stub Utils.calculateNumber', () => {
-    const stub = sinon.stub(Utils, 'calculateNumber').returns(10)
-    const spyLog = sinon.spy(console, "log")
+    const stub = sinon.stub(Utils, 'calculateNumber')
+    const spyLog = sinon.spy(process.stdout, 'write');
+    stub.returns(10);
     sendPaymentRequestToApi(100, 20);
     expect(stub.returnValues).to.deep.equal([10]);
     expect(stub.firstCall.args).to.deep.equal(['SUM', 100, 20])
     expect(stub.calledOnce).to.be.true;
-    expect(spyLog.calledWith('The total is: 10')).to.be.true;
+    expect(spyLog.calledWith('The total is: 10\n')).to.be.true;
+    expect(spyLog.calledOnce).to.be.true;
+    spyLog.restore();
+    stub.restore();
   });
-  afterEach(() => {
-    sinon.restore();
-  })
 })
