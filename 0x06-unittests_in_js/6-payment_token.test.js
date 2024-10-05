388a23033074707cf11a06bb052f68161b247d65
DentistOmarAshraf <omar_ashraf@live.com>
2024-10-05 19:51:52 +0300

    done

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -6,9 +6,9 @@ const getPaymentTokenFromAPI = require('./6-payment_token')
 describe('Testing Async function', () => {
   it('Using done', (done) => {
     getPaymentTokenFromAPI(true)
-      ?.then((data) => {
-        expect(data).to.deep.equal({ data: 'Successful response from the API' });
-        done()
+      .then((data) => {
+        expect(data).to.deep.equal({data: 'Successful response from the API' });
+        done();
       })
       .catch(done);
   })
