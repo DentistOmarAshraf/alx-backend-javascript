42fb3b55bc374ba73f0384257c45f839539df623
DentistOmarAshraf <omar_ashraf@live.com>
2024-10-05 07:29:14 +0300

    [edit] spy restore

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -4,13 +4,13 @@ const { expect } = require('chai');
 const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./3-payment');
 
-describe('testing sinon', () => {
-  it('testing', () => {
+describe('sendPaymentRequestToApi', () => {
+  it('Checking usage of Utils.calculateNumber', () => {
     const spy = sinon.spy(Utils, 'calculateNumber');
     sendPaymentRequestToApi(10, 20);
     expect(spy.calledOnce).to.be.true;
     expect(spy.returnValues).to.deep.equal([30]);
     expect(spy.args[0]).to.deep.equal(['SUM', 10, 20])
-    console.log(spy.args)
+    spy.restore();
   })
 })
